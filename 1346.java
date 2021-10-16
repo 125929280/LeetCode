d2e37f8eef3ffdd3881dc65f069fea53be00244f
zhaozheng <125929280@qq.com>
2021-10-16 13:47:28 +0800

    /

---
+++ 1346.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for(int i:arr) {
+            if(set.contains(i)) return true;
+            set.add(i*2);
+            if(i%2 == 0) set.add(i/2);
+        }
+        return false;
+    }
+}
